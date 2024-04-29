92d78bd1465f02904126ad21b1ee9b1c36ab6d31
Severn Everett <severn.everett@gmail.com>
2024-04-29 13:53:35 +0200

    Added XRangeProcessor

---
+++ src/commonMain/kotlin/com/severett/kemver/processor/XRangeProcessor.kt
@@ -0,0 +1,68 @@
+package com.severett.kemver.processor
+
+import com.severett.kemver.Range
+import com.severett.kemver.Tokenizers.XRANGE
+import com.severett.kemver.processor.RangesUtils.isX
+import com.severett.kemver.processor.RangesUtils.parseIntWithXSupport
+
+object XRangeProcessor : Processor {
+    private val spaceRegex = Regex("\\s+")
+    private val xRangeRegex = Regex(XRANGE)
+
+    override fun process(range: String): String {
+        val ranges = range.split(spaceRegex).mapNotNull { rangeVersion ->
+            xRangeRegex.matchEntire(rangeVersion)?.let { matchResult ->
+                val major = parseIntWithXSupport(matchResult.groupValues[2])
+                val minor = parseIntWithXSupport(matchResult.groupValues[3])
+                val patch = parseIntWithXSupport(matchResult.groupValues[4])
+                val compareSign = matchResult.groupValues[1].takeUnless {
+                    it == Range.RangeOperator.EQ.asString && patch.isX()
+                } ?: ""
+
+                when {
+                    compareSign.isNotBlank() && patch.isX() -> {
+                        when (compareSign) {
+                            Range.RangeOperator.GT.asString -> {
+                                if (minor.isX()) {
+                                    "${Range.RangeOperator.GTE.asString}${major + 1}.0.0"
+                                } else {
+                                    "${Range.RangeOperator.GTE.asString}$major.${minor + 1}.0"
+                                }
+                            }
+
+                            Range.RangeOperator.LTE.asString -> {
+                                if (minor.isX()) {
+                                    "${Range.RangeOperator.LT.asString}${major + 1}.0.0"
+                                } else {
+                                    "${Range.RangeOperator.LT.asString}$major.${minor + 1}.0"
+                                }
+                            }
+
+                            else -> "$compareSign$major.${if (minor.isX()) 0 else minor}.0"
+                        }
+                    }
+
+                    minor.isX() -> {
+                        val from = "${Range.RangeOperator.GTE.asString}$major.0.0"
+                        val to = "${Range.RangeOperator.LT.asString}${major + 1}.0.0"
+                        "$from $to"
+                    }
+
+                    patch.isX() -> {
+                        val from = "${Range.RangeOperator.GTE.asString}$major.$minor.0"
+                        val to = "${Range.RangeOperator.LT.asString}$major.${minor + 1}.0"
+                        "$from $to"
+                    }
+
+                    else -> matchResult.groupValues[0]
+                }
+            }
+        }
+
+        return if (ranges.isEmpty()) {
+            range
+        } else {
+            ranges.joinToString(" ")
+        }
+    }
+}
---
+++ src/commonTest/kotlin/com/severett/kemver/processor/XRangeProcessorTest.kt
@@ -0,0 +1,24 @@
+package com.severett.kemver.processor
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+class XRangeProcessorTest : FunSpec({
+    listOf(
+        ">1.X.X" to ">=2.0.0",
+        ">1.2.X" to ">=1.3.0",
+        "<=1.X.X" to "<2.0.0",
+        "<=1.2.X" to "<1.3.0",
+        ">=1.2.X" to ">=1.2.0",
+        ">=1.X.X" to ">=1.0.0",
+        "1.X" to ">=1.0.0 <2.0.0",
+        "1.2.X" to ">=1.2.0 <1.3.0",
+        "=1.2.X" to ">=1.2.0 <1.3.0",
+        ">=1.2.3 <2.0.0" to ">=1.2.3 <2.0.0",
+        "OTHER" to "OTHER",
+    ).forEach { (range, expectedStr) ->
+        test("Processing x-range expression [$range] should produce string [$expectedStr]") {
+            XRangeProcessor.process(range) shouldBe expectedStr
+        }
+    }
+})
