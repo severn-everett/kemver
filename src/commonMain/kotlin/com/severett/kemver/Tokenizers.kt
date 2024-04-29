9c84dff0f8b0c4d91cd4be09ac8befece801804c
Severn Everett <severn.everett@gmail.com>
2024-04-29 20:20:10 +0200

    Removed Java-dependent string formatting code from Tokenizers

--- src/commonMain/kotlin/com/severett/kemver/Tokenizers.kt
+++ src/commonMain/kotlin/com/severett/kemver/Tokenizers.kt
@@ -1,7 +1,5 @@
 package com.severett.kemver
 
-import java.util.*
-
 internal object Tokenizers {
     /**
      * Not negative integers without leading zeroes.
@@ -17,38 +15,22 @@ internal object Tokenizers {
      *
      * See [semver.org#summary](https://semver.org/#summary)
      */
-    private val MAIN_VERSION = String.format(
-        Locale.ROOT,
-        "(%s)\\.(%s)\\.(%s)",
-        NUMERIC_IDENTIFIER,
-        NUMERIC_IDENTIFIER,
-        NUMERIC_IDENTIFIER,
-    )
+    private const val MAIN_VERSION = "($NUMERIC_IDENTIFIER)\\.($NUMERIC_IDENTIFIER)\\.($NUMERIC_IDENTIFIER)"
 
-    private val PRERELEASE_IDENTIFIER = String.format(
-        Locale.ROOT,
-        "(?:%s|%s)",
-        NUMERIC_IDENTIFIER,
-        NON_NUMERIC_IDENTIFIER,
-    )
+    private const val PRERELEASE_IDENTIFIER = "(?:$NUMERIC_IDENTIFIER|$NON_NUMERIC_IDENTIFIER)"
 
     /**
      * Prerelease identifier forwarded by hyphen with appended series of dot-separated identifiers.
      *
      * See [semver.org#spec-item-9](https://semver.org/#spec-item-9)
      */
-    private val PRERELEASE = String.format(
-        Locale.ROOT,
-        "(?:-(%s(?:\\.%s)*))",
-        PRERELEASE_IDENTIFIER,
-        PRERELEASE_IDENTIFIER,
-    )
+    private const val PRERELEASE = "(?:-($PRERELEASE_IDENTIFIER(?:\\.$PRERELEASE_IDENTIFIER)*))"
 
     private const val BUILD_IDENTIFIER = "[0-9A-Za-z-]+"
 
-    private val BUILD = String.format(Locale.ROOT, "(?:\\+(%s(?:\\.%s)*))", BUILD_IDENTIFIER, BUILD_IDENTIFIER)
+    private const val BUILD = "(?:\\+($BUILD_IDENTIFIER(?:\\.$BUILD_IDENTIFIER)*))"
 
-    private val STRICT_PLAIN = String.format(Locale.ROOT, "v?%s%s?%s?", MAIN_VERSION, PRERELEASE, BUILD)
+    private const val STRICT_PLAIN = "v?$MAIN_VERSION$PRERELEASE?$BUILD?"
 
     /**
      * Fully, completely valid semver value.
@@ -65,38 +47,30 @@ internal object Tokenizers {
      * X.Y.X-beta.1+sha1234
      * ```
      */
-    val STRICT = String.format(Locale.ROOT, "^%s$", STRICT_PLAIN)
+    const val STRICT = "^$STRICT_PLAIN$"
 
     /**
      * `x`, `X`, and `*` are for X-Ranges.
      *
      * `+` is for Ivy ranges.
      */
-    private val XRANGE_IDENTIFIER = String.format(Locale.ROOT, "%s|x|X|\\*|\\+", NUMERIC_IDENTIFIER)
+    private const val XRANGE_IDENTIFIER = "$NUMERIC_IDENTIFIER|x|X|\\*|\\+"
 
-    private val XRANGE_PLAIN = String.format(
-        Locale.ROOT,
-        "[v=\\s]*(%s)(?:\\.(%s)(?:\\.(%s)(?:%s)?%s?)?)?",
-        XRANGE_IDENTIFIER,
-        XRANGE_IDENTIFIER,
-        XRANGE_IDENTIFIER,
-        PRERELEASE,
-        BUILD,
-    )
+    private const val XRANGE_PLAIN = "[v=\\s]*($XRANGE_IDENTIFIER)(?:\\.($XRANGE_IDENTIFIER)(?:\\.($XRANGE_IDENTIFIER)(?:$PRERELEASE)?$BUILD?)?)?"
 
     private const val LONE_CARET = "(?:\\^)"
 
-    val CARET = String.format(Locale.ROOT, "^%s%s$", LONE_CARET, XRANGE_PLAIN)
+    const val CARET = "^$LONE_CARET$XRANGE_PLAIN$"
 
-    val HYPHEN = String.format(Locale.ROOT, "^\\s*(%s)\\s+-\\s+(%s)\\s*$", XRANGE_PLAIN, XRANGE_PLAIN)
+    const val HYPHEN = "^\\s*($XRANGE_PLAIN)\\s+-\\s+($XRANGE_PLAIN)\\s*$"
 
     const val IVY = "^(\\[|\\]|\\()([0-9]+)?\\.?([0-9]+)?\\.?([0-9]+)?\\,([0-9]+)?\\.?([0-9]+)?\\.?([0-9]+)?(\\]|\\[|\\))$"
 
-    val TILDE = String.format(Locale.ROOT, "^(?:~>?)%s$", XRANGE_PLAIN)
+    const val TILDE = "^(?:~>?)$XRANGE_PLAIN$"
 
     private const val GLTL = "((?:<|>)?=?)"
 
-    val XRANGE = String.format(Locale.ROOT, "^%s\\s*%s$", GLTL, XRANGE_PLAIN)
+    const val XRANGE = "^$GLTL\\s*$XRANGE_PLAIN$"
 
-    val COMPARATOR = String.format(Locale.ROOT, "^%s\\s*(%s)$|^$", GLTL, STRICT_PLAIN)
+    const val COMPARATOR = "^$GLTL\\s*($STRICT_PLAIN)$|^$"
 }
